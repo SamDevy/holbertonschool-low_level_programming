328674e85a3293a0108cccec53a08911cf44a2fc
Hussam <SamDevy@outlook.com>
2025-08-25 21:44:48 +0000

    task 1

---
+++ hello_world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This function is the entry point of the program.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("hello_world");
+}
