f8d844e22b47c46db1351dd459509bb816cd9008
Hussam <SamDevy@outlook.com>
2025-08-27 17:09:31 +0000

    ff

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,8 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
- * followed by a new line using printf.
+ * Description: Prints a message using printf.
  *
  * Return: Always 0 (Success)
  */
