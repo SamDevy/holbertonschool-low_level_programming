caba0dd2cdaf4929f38ccb0cdf4df44fcd17d177
Hussam <samdevy@outlook.com>
2025-09-01 12:52:38 +0300

    Update 0-positive_or_negative.c
    simple clean

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,37 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
  * main - Entry point
  *
- * Description: Prints whether a random number is positive, zero, or negative
+ * Description: Generates a random number, prints its last digit,
+ * and describes if it's greater than 5, 0, or less than 6 and not 0.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
+	int a;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-		if (n > 0)
+	a = n % 10;
+
+	/* Blank line after declarations above */
+
+	if (a > 5)
 	{
-		printf("%d is positive\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
-	else if (n == 0)
+	else if (a == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
+
 	return (0);
 }
