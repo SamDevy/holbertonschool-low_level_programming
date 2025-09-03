19ab1b221ac3dc892f4bc466a2aeaa4749ec908e
Hussam <samdevy@outlook.com>
2025-09-03 14:05:02 +0300

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
-/*
- * Prints digits 0-9
- * Uses putchar in while loop
- * Outputs a newline at end
- * Returns from main
- * succed 0
+/**
+ * main - Prints digits 0 to 9
+ *
+ * Uses putchar in a while loop to print digits from 0 to 9,
+ * then prints a newline at the end.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
+	char to_9 = '0';
 
-char to_9 = '0';
+	while (to_9 <= '9')
+	{
+		putchar(to_9);
+		to_9++;
+	}
+	putchar('\n');
 
-while(to_9 <= '9')
-{
-    putchar(to_9);
-    to_9++;
+	return (0);
 }
-putchar('\n');
-
-return 0;
-}
-
