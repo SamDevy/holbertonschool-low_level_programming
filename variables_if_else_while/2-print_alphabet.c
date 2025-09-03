8630f079022dbb4301db4770df404590c21d3224
Hussam <samdevy@outlook.com>
2025-09-03 11:36:56 +0300

    Update 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,20 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-/*
- * Prints lowercase letters 'a' to 'z' to standard output.
- * Starts from character 'a' and increments until 'z'.
- * Uses a while loop and putchar for output.
- * Outputs letters consecutively without spaces.
- * Exits with status code 0.
+/**
+ * main - Prints lowercase letters a to z
+ *
+ * Return: Always 0 (Success)
  */
-int main (void) {
+int main(void)
+{
+	char a = 'a';
 
-    char a = 'a';
-    while (a <= 'z') {
-    putchar(a);
-    a++;
-}
-putchar('\n');
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
 
-return 0;
+	return (0);
 }
