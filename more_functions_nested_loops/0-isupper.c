0076269ea8705740dd445fe32971d5b64fc6487f
Hussam <samdevy@outlook.com>
2025-09-09 15:47:11 +0300

    Update 0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 /* 0-isupper.c */
 
 /**
- * _isupper - Checks for uppercase character.
+ * _isupper - Checks if a character is uppercase.
  * @c: The character to check.
  *
  * Return: 1 if c is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
-    /* Return 1 if c is between 'A' and 'Z', else 0 */
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    return (0);
+	/* Check if c is between 'A' and 'Z' in ASCII */
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
