5054780491122a841c90ccaa8dae0c4529f49d71
Hussam <samdevy@outlook.com>
2025-09-09 15:52:54 +0300

    Create 3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/* 3-print_numbers.c */
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+ *
+ * Return: Nothing.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	/* Print numbers from 0 to 9 */
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	/* Print new line */
+	_putchar('\n');
+}
