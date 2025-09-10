149218bee18b6e405619d95192e817dfc1afea7f
Hussam <samdevy@outlook.com>
2025-09-11 00:14:55 +0300

    Create 9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+/* 9-strcpy.c - Way 1 */
+/**
+ * _strcpy - Copies the string pointed to by src to dest
+ * @dest: The destination buffer
+ * @src: The source string
+ *
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	/* Copy each character including the null terminator */
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
