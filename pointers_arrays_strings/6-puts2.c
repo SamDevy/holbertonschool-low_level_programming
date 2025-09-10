16b0cc152f19f1844936de541f9973c4651b13eb
Hussam <samdevy@outlook.com>
2025-09-11 00:39:17 +0300

    Create 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first
+ * @str: The string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	/* Print every other character */
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			printf("%c", str[i]);
+	}
+	printf("\n");
+}
