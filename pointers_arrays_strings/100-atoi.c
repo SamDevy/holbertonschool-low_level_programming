f344d2f959f22fca2cb8497e1cbc3da4f58c0072
Hussam <samdevy@outlook.com>
2025-09-11 00:05:29 +0300

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,21 @@
 /* 100-atoi.c */
 /**
- * _atoi - Converts a string to an integer
+ * convert_digits - Converts digit characters to integer
  * @s: The string to convert
+ * @i: The starting index
+ * @sign: The sign of the number
  *
- * Return: The converted integer, or 0 if no numbers found
+ * Return: The converted integer
  */
-int _atoi(char *s)
+int convert_digits(char *s, int i, int sign)
 {
-	int i = 0;
-	int sign = 1;
 	int result = 0;
 	int started = 0;
 
-	/* Process all characters in the string */
 	while (s[i])
 	{
-		/* Update sign for each '-' before digits */
-		if (s[i] == '-' && !started)
-			sign *= -1;
-		else if (s[i] == '+' && !started)
-			;
-		else if (s[i] >= '0' && s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			/* Start building the number as negative to avoid overflow */
 			if (!started)
 			{
 				started = 1;
@@ -30,7 +23,6 @@ int _atoi(char *s)
 			}
 			else
 			{
-				/* Check for overflow before multiplying and subtracting */
 				if (result < (-2147483647 / 10))
 					return (sign == 1 ? 2147483647 : -2147483648);
 				result = result * 10 - (s[i] - '0');
@@ -42,13 +34,35 @@ int _atoi(char *s)
 	}
 	if (!started)
 		return (0);
-	/* If sign is positive, flip result to positive */
 	if (sign == 1)
 	{
-		/* Handle INT_MIN edge case */
 		if (result == -2147483648)
 			return (2147483647);
 		return (-result);
 	}
 	return (result);
+}
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
+ *
+ * Return: The converted integer, or 0 if no numbers found
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+
+	while (s[i])
+	{
+		if (s[i] == '-' && (s[i + 1] < '0' || s[i + 1] > '9'))
+			sign *= -1;
+		else if (s[i] == '+' && (s[i + 1] < '0' || s[i + 1] > '9'))
+			;
+		else if (s[i] >= '0' && s[i] <= '9')
+			break;
+		i++;
+	}
+	return (convert_digits(s, i, sign));
 }
