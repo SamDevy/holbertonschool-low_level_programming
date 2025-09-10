fd4a27811719340b3a70878450ecb793c9e99e07
Hussam <samdevy@outlook.com>
2025-09-11 00:20:29 +0300

    Create 8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+/* 8-print_array.c - Way 1 */
+/**
+ * print_array - Prints n elements of an array of integers
+ * @a: The array of integers
+ * @n: The number of elements to print
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	/* Print each element separated by comma and space */
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
