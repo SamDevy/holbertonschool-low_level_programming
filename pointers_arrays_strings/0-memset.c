f7412f76b634b654f632a7b87415107515df6407
Hussam <samdevy@outlook.com>
2025-09-13 19:13:47 +0300

    Update 0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -10,28 +10,29 @@
 /* Way 1: Using array indexing */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b; /* Fill each byte with value b */
-    }
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b; /* Fill each byte with value b */
+	}
 
-    return (s);
+	return (s);
 }
 
-/* Way 2: Using pointer arithmetic */
 /*
-char *_memset(char *s, char b, unsigned int n)
-{
-    unsigned int i;
-    char *p = s;
-
-    for (i = 0; i < n; i++)
-    {
-        *(p + i) = b; // Fill each byte with value b
-    }
-
-    return (s);
-}
-*/
+ * Way 2: Using pointer arithmetic
+ *
+ * char *_memset(char *s, char b, unsigned int n)
+ * {
+ *	unsigned int i;
+ *	char *p = s;
+ *
+ *	for (i = 0; i < n; i++)
+ *	{
+ *		*(p + i) = b; /* Fill each byte with value b */
+ *	}
+ *
+ *	return (s);
+ * }
+ */
