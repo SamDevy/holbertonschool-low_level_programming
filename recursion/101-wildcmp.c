d51a7631e989b849c7fb505acf1cd0a9b89be13e
Hussam <samdevy@outlook.com>
2025-09-13 23:05:29 +0300

    Update 101-wildcmp.c

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
+ * skip_stars - skips consecutive '*' characters in s2
+ * @s2: string pointer
+ *
+ * Return: pointer to first non-'*' character
+ */
+char *skip_stars(char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) == '*')
+		return (skip_stars(s2 + 1));
+	return (s2);
+}
+
+/**
  * wildcmp - compares two strings and returns 1 if identical
  * @s1: first string
  * @s2: second string (may contain *)
@@ -9,22 +22,16 @@
  */
 int wildcmp(char *s1, char *s2)
 {
+	s2 = skip_stars(s2);
+
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
 	if (*s2 == '*')
-	{
-		/* Skip consecutive '*' */
-		while (*(s2 + 1) == '*')
-			s2++;
-
-		/* '*' matches empty string or one char in s1 */
-		if (wildcmp(s1, s2 + 1))
-			return (1);
-		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
-			return (1);
-		return (0);
-	}
+		return (
+			wildcmp(s1, s2 + 1) || /* '*' matches empty */
+			(*s1 != '\0' && wildcmp(s1 + 1, s2)) /* '*' matches one char */
+		);
 
 	if (*s1 == *s2 && *s1 != '\0')
 		return (wildcmp(s1 + 1, s2 + 1));
