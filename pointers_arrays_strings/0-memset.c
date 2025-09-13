477c9465b595321f959758546730df8bf7017a7b
Hussam <samdevy@outlook.com>
2025-09-13 19:11:04 +0300

    Update 0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,3 @@
-/* 0-memset.c */
-
 /**
  * _memset - fills memory with a constant byte
  * @s: pointer to the memory area to fill
@@ -7,18 +5,33 @@
  * @n: number of bytes to fill
  *
  * Return: pointer to the memory area s
- *
- * Way 1: Using a for loop with array indexing
  */
+
+/* Way 1: Using array indexing */
 char *_memset(char *s, char b, unsigned int n)
 {
     unsigned int i;
 
-    /* Fill each byte of the memory area with the value of b */
     for (i = 0; i < n; i++)
     {
-        s[i] = b;
+        s[i] = b; /* Fill each byte with value b */
     }
 
     return (s);
 }
+
+/* Way 2: Using pointer arithmetic */
+/*
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i;
+    char *p = s;
+
+    for (i = 0; i < n; i++)
+    {
+        *(p + i) = b; // Fill each byte with value b
+    }
+
+    return (s);
+}
+*/
