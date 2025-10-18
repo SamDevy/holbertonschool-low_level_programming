60e1971b998c517492d7ba8f796a63c5b95e3f5a
Hussam <samdevy@outlook.com>
2025-10-18 20:25:37 +0300

    Add header file for singly linked list implementation

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+/* lists.h */
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list node
+ * @str: string (malloc'ed)
+ * @len: length of the string
+ * @next: pointer to next node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
