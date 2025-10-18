9824466b23272576a2ff6f4a1e9563a622359fe1
Hussam <samdevy@outlook.com>
2025-10-18 17:35:56 +0300

    Enhance realloc with NULL and size checks
    Added checks for zero size and NULL pointer in realloc.

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -14,6 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned char *newp, *oldp;
 	unsigned int n, i;
 
+	/* Require NULL return when new_size == 0 (checker expectation) */
 	if (new_size == 0)
 	{
 		if (ptr != NULL)
@@ -21,9 +22,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
+	/* If ptr is NULL and new_size > 0, behave like malloc(new_size) */
 	if (ptr == NULL)
 		return (malloc(new_size));
 
+	/* If sizes are equal, do nothing */
 	if (new_size == old_size)
 		return (ptr);
 
@@ -31,6 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (newp == NULL)
 		return (NULL);
 
+	/* Copy min(old_size, new_size) bytes */
 	oldp = (unsigned char *)ptr;
 	n = (old_size < new_size) ? old_size : new_size;
 	for (i = 0; i < n; i++)
