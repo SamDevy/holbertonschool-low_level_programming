c7377ccf1d8fc5595b34491f0d44dd9154f944c4
Hussam <samdevy@outlook.com>
2025-10-18 20:24:17 +0300

    Create 0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+/* 0-print_list.c */
+/**
+ * print_list - Prints all elements of a list_t linked list
+ * @h: head pointer to the list
+ *
+ * Return: number of nodes
+ *
+ * Behavior:
+ * - Prints "[len] str" per node.
+ * - If str is NULL, prints "[0] (nil)".
+ * - Uses printf as allowed by the task.
+ */
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
