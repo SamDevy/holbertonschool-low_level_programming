562da0b327bd06f94a673fcb81f3aeb77f405c34
Hussam <samdevy@outlook.com>
2025-10-19 04:01:10 +0300

    Update lists.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -9,6 +9,8 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
  */
 typedef struct dlistint_s
 {
@@ -20,5 +22,10 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+/* Add others as you implement more tasks:
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+void free_dlistint(dlistint_t *head);
+*/
 
 #endif /* LISTS_H */
