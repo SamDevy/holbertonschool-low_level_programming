a375253aaca8a95896a7a590b07c7025f18aac7e
Hussam <samdevy@outlook.com>
2025-10-19 03:55:08 +0300

    Enhance comments in lists.sh for clarity
    Added a description for the doubly linked list node structure and commented out function prototypes.

--- doubly_linked_lists/lists.sh
+++ doubly_linked_lists/lists.sh
@@ -9,6 +9,8 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
  */
 typedef struct dlistint_s
 {
@@ -18,8 +20,10 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+/* If you need them for your other tasks, keep prototypes here too:
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+*/
 
 #endif /* LISTS_H */
