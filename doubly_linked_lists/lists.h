9c385ba26e730970dcb3962c27c6150f54f3d181
Hussam <samdevy@outlook.com>
2025-10-19 03:57:07 +0300

    Update lists.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -20,10 +20,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-/* If you need them for your other tasks, keep prototypes here too:
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-void free_dlistint(dlistint_t *head);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-*/
+size_t dlistint_len(const dlistint_t *h);
 
 #endif /* LISTS_H */
