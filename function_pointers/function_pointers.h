410f06edd53269b143f82e61807a7c3516740d8d
Hussam <samdevy@outlook.com>
2025-10-20 19:10:22 +0300

    Create function_pointers.h

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * This file contains the function prototypes for the 
+ * 0x0E. C - Function pointers project.
+ */
+
+#include <stddef.h> /* This includes NULL */
+
+/* 0. What's my name */
+void print_name(char *name, void (*f)(char *));
+
+
+#endif /* FUNCTION_POINTERS_H */
