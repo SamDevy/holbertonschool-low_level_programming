67668e8eec27071f24450c00f4cff8bb5b8e0568
Hussam <samdevy@outlook.com>
2025-10-20 18:46:40 +0300

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,4 @@
+/* dog.h */
 #ifndef DOG_H
 #define DOG_H
 
@@ -16,8 +17,10 @@ struct dog
 	char *owner;
 };
 
+/* typedef alias for struct dog */
 typedef struct dog dog_t;
 
+/* Prototypes for tasks */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
