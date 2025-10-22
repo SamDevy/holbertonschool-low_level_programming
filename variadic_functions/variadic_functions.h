964fbbe5704f3f5fb89d39eefd58b3c3464664bb
Hussam <samdevy@outlook.com>
2025-10-22 04:30:53 +0300

    Create variadic_functions.h

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/**
+ * Desc: Header file containing prototypes for all functions
+ * used in the variadic_functions directory.
+ */
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
