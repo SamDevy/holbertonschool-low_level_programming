35a80bc19ed429a8e4c89a7e4e113b6574356c0e
Hussam <samdevy@outlook.com>
2025-10-22 04:35:51 +0300

    Update variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,11 +4,21 @@
 #include <stdarg.h>
 
 /**
- * Desc: Header file containing prototypes for all functions
- * used in the variadic_functions directory.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c);
+
+/**
+ * sum_them_all - calculates the sum of all its parameters.
+ * @n: The number of parameters passed to the function.
+ * @...: A variable number of parameters to calculate the sum of.
+ *
+ * Return: If n == 0, return 0. Otherwise, return the sum of all parameters.
+ */
 int sum_them_all(const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
