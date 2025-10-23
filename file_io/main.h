2a079327a6323bd8ba10451e89bda4f831d31bd7
Hussam <samdevy@outlook.com>
2025-10-23 18:46:02 +0300

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,8 +1,10 @@
+/* main.h */
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
