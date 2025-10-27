2e4d80279fe2c6847103936650cb46b98c91817c
Hussam <samdevy@outlook.com>
2025-10-27 17:43:55 +0300

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,11 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stddef.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+
+/* 0. Tread lightly, she is near */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* 1. Under the snow */
+int create_file(const char *filename, char *text_content);
+
+/* 2. Speak gently, she can hear */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* 3. cp */
+int main(int ac, char **av);
 
 #endif /* MAIN_H */
