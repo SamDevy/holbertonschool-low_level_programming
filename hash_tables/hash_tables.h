4802ea86ecbf98ea39db0c662bcc7a11d6bb16dc
Hussam <samdevy@outlook.com>
2025-10-30 11:06:52 +0300

    Update hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -9,10 +9,6 @@
  * @key: The key, string (unique in the table)
  * @value: The value corresponding to a key
  * @next: Pointer to the next node (for chaining)
- *
- * Description:
- * Each node stores a key/value pair. Collisions are handled
- * by separate chaining, linking nodes in the same bucket.
  */
 typedef struct hash_node_s
 {
@@ -25,10 +21,6 @@ typedef struct hash_node_s
  * struct hash_table_s - Hash table data structure
  * @size: The size of the array
  * @array: Array of pointers to bucket heads
- *
- * Description:
- * Each array cell points to the head of a linked list of
- * hash_node_t elements. Collisions are handled by chaining.
  */
 typedef struct hash_table_s
 {
@@ -36,7 +28,7 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-/* Creates a new hash table with the given size. */
+/* Create a hash table with a given size */
 hash_table_t *hash_table_create(unsigned long int size);
 
 #endif /* HASH_TABLES_H */
