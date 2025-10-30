06e2a905c1a327c93a514bf77ff5874d598e4c15
Hussam <samdevy@outlook.com>
2025-10-30 12:30:54 +0300

    Update hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -7,31 +7,33 @@
 
 /**
  * struct hash_node_s - Node of a hash table
- * @key: key string
- * @value: value associated with the key
- * @next: pointer to next node in the same index
+ * @key: key, string, unique in the hash table
+ * @value: value corresponding to a key
+ * @next: points to the next node in the same index
  */
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
  * @size: size of the array
- * @array: array of pointers to hash nodes
+ * @array: array of size @size, each element is a pointer to the first node
  */
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
+/* Function prototypes */
+hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-hash_table_t *hash_table_create(unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif /* HASH_TABLES_H */
