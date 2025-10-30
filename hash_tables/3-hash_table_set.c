3567772249986e75110e50d505e152a3ec7dbcab
Hussam <samdevy@outlook.com>
2025-10-30 12:28:05 +0300

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,29 +9,29 @@
  */
 hash_node_t *create_node(const char *key, const char *value)
 {
-    hash_node_t *node;
+	hash_node_t *node;
 
-    node = malloc(sizeof(hash_node_t));
-    if (!node)
-        return (NULL);
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
 
-    node->key = strdup(key);
-    if (!node->key)
-    {
-        free(node);
-        return (NULL);
-    }
+	node->key = strdup(key);
+	if (!node->key)
+	{
+		free(node);
+		return (NULL);
+	}
 
-    node->value = strdup(value);
-    if (!node->value)
-    {
-        free(node->key);
-        free(node);
-        return (NULL);
-    }
+	node->value = strdup(value);
+	if (!node->value)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
 
-    node->next = NULL;
-    return (node);
+	node->next = NULL;
+	return (node);
 }
 
 /**
@@ -43,15 +43,15 @@ hash_node_t *create_node(const char *key, const char *value)
  */
 int update_node(hash_node_t *node, const char *value)
 {
-    char *new_value;
+	char *new_value;
 
-    new_value = strdup(value);
-    if (!new_value)
-        return (0);
+	new_value = strdup(value);
+	if (!new_value)
+		return (0);
 
-    free(node->value);
-    node->value = new_value;
-    return (1);
+	free(node->value);
+	node->value = new_value;
+	return (1);
 }
 
 /**
@@ -64,28 +64,28 @@ int update_node(hash_node_t *node, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *node, *current;
+	unsigned long int index;
+	hash_node_t *node, *current;
 
-    if (!ht || !key || !*key || !value)
-        return (0);
+	if (!ht || !key || !*key || !value)
+		return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    current = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
 
-    while (current)
-    {
-        if (strcmp(current->key, key) == 0)
-            return (update_node(current, value));
-        current = current->next;
-    }
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (update_node(current, value));
+		current = current->next;
+	}
 
-    node = create_node(key, value);
-    if (!node)
-        return (0);
+	node = create_node(key, value);
+	if (!node)
+		return (0);
 
-    node->next = ht->array[index];
-    ht->array[index] = node;
+	node->next = ht->array[index];
+	ht->array[index] = node;
 
-    return (1);
+	return (1);
 }
