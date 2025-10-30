65bf35c583ceb204cacb9f4579485f6548ae235c
Hussam <samdevy@outlook.com>
2025-10-30 12:22:23 +0300

    Update hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,27 +1,89 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
-
 #include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
 
-/* Node of a hash table */
-typedef struct hash_node_s
+/**
+ * update_node - Updates the value of an existing hash node
+ * @node: Pointer to the node to update
+ * @value: New value to set
+ *
+ * Return: 1 on success, 0 on failure
+ */
+static int update_node(hash_node_t *node, const char *value)
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
-} hash_node_t;
+	char *new_value;
 
-/* Hash table structure */
-typedef struct hash_table_s
+	new_value = strdup(value);
+	if (!new_value)
+		return (0);
+
+	free(node->value);
+	node->value = new_value;
+	return (1);
+}
+
+/**
+ * create_node - Creates a new hash node with a key and value
+ * @key: The key string
+ * @value: The value string
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
+static hash_node_t *create_node(const char *key, const char *value)
 {
-	unsigned long int size;
-	hash_node_t **array;
-} hash_table_t;
+	hash_node_t *new_node;
 
-/* Prototypes */
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (NULL);
 
-#endif /* HASH_TABLES_H */
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
+ * hash_table_set - Adds or updates an element in a hash table
+ * @ht: Pointer to the hash table
+ * @key: The key (cannot be an empty string)
+ * @value: The value associated with the key
+ *
+ * Return: 1 if success, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *temp, *new_node;
+
+	if (!ht || !key || !*key || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (update_node(temp, value));
+		temp = temp->next;
+	}
+
+	new_node = create_node(key, value);
+	if (!new_node)
+		return (0);
+
+	new_node->n_
